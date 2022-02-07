8d88a3038c330f73948304d0df99038953109a37
Vivek Kumar <48044537+vivekuser@users.noreply.github.com>
2022-02-07 08:04:00 +0530

    Add files via upload

---
+++ Totalsalary.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class TotalSalary\ {
+	
+	public static void main(String[] args) {
+		// Write your code here
+       Scanner s=new Scanner(System.in);
+		int bs=s.nextInt();
+		String str=s.next();
+		char c=str.charAt(0);
+		int all;
+		if(c==65)
+		{
+			all=1700;
+		}
+		else if(c==66)
+		{
+			all=1500;
+		}
+		else
+		{
+			all=1300;
+		}
+		double ts=((bs+(0.20*bs)+(0.50*bs)+all)-(0.11*bs));
+		double p=Math.round(ts);
+		int h=(int)(p);
+		System.out.println(h);
+	}
+}
